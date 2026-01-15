321acb0132ca106d83e4bdfcf663d9086bc945f3
ismaelmunoz <ismaelprogramador852@gmail.com>
2026-01-14 18:00:17 -0600

    Pruebas

--- src/bases_ts/basic.ts
+++ src/bases_ts/basic.ts
@@ -1,16 +1,56 @@
-export const name = "Hola mundo"
+export const name = ""
 
 export const lugar = false;
 
-export function f(){
-    const message = "Hello world";
-
+export function f(message: string){
     return message
 }
 
 export function suma(a: number, b: number): number{
 
     return a+b
+
+}
+
+export const NombrePersona = (nombre: string, apellido: string) => {
+
+
+    if(nombre === "ISMAEL" && apellido === "MUÑOZ"){
+        return "ESO";
+    }else{
+        return "Hola mundo"
+    }
+
+
+}
+
+export const login = (usuario: string, contra: string) =>{
+
+
+    //Esto lo hice yo. Esta mal hecho, que funciona si, pero mal hecho
+    // if(usuario == "imunoz" || contra == "12345"){
+    //     return "Accesos concedido"
+    // }else if(usuario == "" || contra == ""){
+    //     return "CAMPOS OBLIGATORIOS"
+    // }else{
+    //     return "ACCESO DENEGADO"
+    // }
+
+    if(usuario === "" || contra === ""){
+        return "CAMBOS OBLIGATORIOS"
+    }
+    else if(usuario === "imunoz" && contra === "1234"){
+        return "ACCESO CONCEDIDIO"
+    }else{
+        return "ACCESO DENEGADO"
+    }
+
+
+}
+
+export const precios = (saldo: number, precio: number, estado: string) =>{
+
+    
 
 
 }
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 
-import { name } from "./bases_ts/basic.ts";
-import { f } from "./bases_ts/basic.ts"
-import { suma } from "./bases_ts/basic.ts"
+import { name, /*suma, NombrePersona, login*/ } from "./bases_ts/basic.ts";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -16,33 +14,87 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </a>
     
     <h1>${name}</h1>
-    <h2>${f()}</h2>
 
-    <div class="contenedor-suma">
+    <!--<div class="contenedor-suma">
       <input class="input-input" id="num1" type="number"/>
       <input class = "input-input" id="num2" type="number"/>
       <button class="boton-contenedor" id="btn">Sumar</button>
 
 
       <h2 id="resultado"></h2>
-      <h2 id="holamundo"></h2>
-    
-    </div>
+      </div>
+
+      <div class="contenedor-nombre">
+        <input class="input-nombre-apellido" id="nombre" type="text"/>
+        <input class="input-nombre-apellido" id="apellido" type="text"/>
+        
+        <h4 id="verdadero-falso"></h4>
+
+      </div> NOMBRE Y APELLIDO, SI ES VERDADERO O SI ES FALSO -->
+
+      <!-- <div class="contenedor-login">
+
+        <input class="contenedor-input" id="usuario" placeHolder="Ingrese Usuario"/>
+        <input class="contenedor-input" id="contraseña" placeHolder="Ingrese Contraseña"/>
+
+        <button class="button-entrada" id="btn-login" >Ingresar</button>
+
+        <h5 id="acceso"></h5>
+
+
+      </div> --> <!--LOGIN-->
+
+      <div class="contenedor-ejercicio3">
+
+        <div class="contenedor-label-input">
+        <label class="label-label">Ingrese el Saldo</label>
+        <input class="input-input" id="saldo" placeholder="Ingrese el saldo" type="number">
+        </div>
+        <div class="contenedor-label-input">
+          <label class="label-label">Precio Producto</label>
+          <input class="input-input" id="precio" placeholder="Ingrese el precio del Producto" type="number">
+        </div>
+        <div class="contenedor-label-input-3">
+          <input class="radio-button" id="vip" type="radio">
+          <label for="vip">VIP</label>
+          <input class="radio-button" id="no-vip" type="radio">
+          <label for="no-vip">NO VIP</label>
+        </div>
+      </div>
+
+      <button class="button-login">INGRESAR</button>
+
+      <h5 id="resultado-venta"></h5>
+
   </div>
 `
 
-const btn = document.querySelector<HTMLButtonElement>("#btn")!
-const resultado = document.querySelector<HTMLButtonElement>("#resultado")!
-const hola = document.querySelector<HTMLLabelElement>("#holamundo")!
+// const btn = document.querySelector<HTMLButtonElement>("#btn")!
+// const resultado = document.querySelector<HTMLHeadingElement>("#resultado")!
+// const resultado_nombre = document.querySelector<HTMLHeadingElement>("#verdadero-falso")!
 
-btn.addEventListener("click", () =>{
-  const n1 = Number(document.querySelector<HTMLInputElement>("#num1")!.value)
-  const n2 = Number(document.querySelector<HTMLInputElement>("#num2")!.value)
-  const n3 = String(document.querySelector<HTMLLabelElement>("#holamundo")!)
+// btn.addEventListener("click", () =>{
+//   const n1 = Number(document.querySelector<HTMLInputElement>("#num1")!.value)
+//   const n2 = Number(document.querySelector<HTMLInputElement>("#num2")!.value)
+//   const nombre_persona = String(document.querySelector<HTMLInputElement>("#nombre")!.value)
+//   const apellido_persona = String(document.querySelector<HTMLInputElement>("#apellido")!.value)
 
-  resultado.textContent = suma(n1, n2).toString();
-  hola.textContent = n3;
+//   resultado.textContent = suma(n1, n2).toString();
+
+//   resultado_nombre.textContent = NombrePersona(nombre_persona, apellido_persona); 
+
+// }) NOMBRE DE LA PERSONA - SUMA
+
+// const btn = document.querySelector<HTMLButtonElement>("#btn-login")!
+// const acceso = document.querySelector<HTMLHeadingElement>("#acceso")!
+
+// btn.addEventListener("click", () =>{
+
+//   const us = String(document.querySelector<HTMLInputElement>("#usuario")!.value)
+//   const contra = String(document.querySelector<HTMLInputElement>("#contraseña")!.value)
+
+//   acceso.textContent = login(us, contra)
+// }) LOGIN - ACCESO DENEGADO O NO
 
 
-})
 
